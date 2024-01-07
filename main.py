e131dd4199f07fa9ecbc845dd72102b032261736
Anjana Urulugastenna <f.anju6448@gmail.com>
2024-01-07 10:08:37 +0530

    Adding footer

--- main.py
+++ main.py
@@ -16,7 +16,34 @@ with st.sidebar:
                                             value=365,
                                             key="forcasting_period")
         submit_button = st.form_submit_button(label="submit")
-        
+
+st.markdown("""
+    <style>
+    .reportview-container .main .block-container {
+        max-width: 1200px;
+        padding-top: 2rem;
+        padding-right: 2rem;
+        padding-left: 2rem;
+        padding-bottom: 2rem;
+    }
+    .footer {
+        position: fixed;
+        left: 0;
+        bottom: 0;
+        width: 100%;
+        background-color: #f5f5f5;
+        color: black;
+        text-align: center;
+    }
+    </style>
+    """, unsafe_allow_html=True)
+
+st.markdown("""
+    <div class="footer">
+        <p>Copyright © 2024 Anjana Urulugastenna. All rights reserved.</p>
+    </div>
+    """, unsafe_allow_html=True)
+
 if submit_button:
     predictor = sf.StockPredictor(stock_symbol, start_date, end_date, forcasting_period)
     
